abc95ee72fe8093a626f85d5bd09f7d43cb58e69
Bernhard <bernhard@beast>
2024-03-03 15:37:20 +0100

    feat: add config for nvim and kitty

---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,96 @@
+-- Install Packer automatically if it's not installed(Bootstraping)
+-- Hint: string concatenation is done by `..`
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+local packer_bootstrap = ensure_packer()
+
+
+-- Reload configurations if we modify plugins.lua
+-- Hint
+--     <afile> - replaced with the filename of the buffer being manipulated
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+
+-- Install plugins here - `use ...`
+-- Packer.nvim hints
+--     after = string or list,           -- Specifies plugins to load before this plugin. See "sequencing" below
+--     config = string or function,      -- Specifies code to run after this plugin is loaded
+--     requires = string or list,        -- Specifies plugin dependencies. See "dependencies". 
+--     ft = string or list,              -- Specifies filetypes which load this plugin.
+--     run = string, function, or table, -- Specify operations to be run after successful installs/updates of a plugin
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    ---------------------------------------
+    -- NOTE: PUT YOUR THIRD PLUGIN HERE --
+    ---------------------------------------
+
+    -- theme
+    use 'navarasu/onedark.nvim'
+
+    -- code auto complete
+    use { 'williamboman/mason.nvim' }
+    use { 'williamboman/mason-lspconfig.nvim'}
+    use { 'neovim/nvim-lspconfig' }
+    use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }    
+    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' } 
+    use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }        -- buffer auto-completion
+    use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }          -- path auto-completion
+    use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' }       -- cmdline auto-completion
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+    use { 'nvim-lualine/lualine.nvim' }
+
+    use { "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      requires = { 
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        {
+          's1n7ax/nvim-window-picker',
+          version = '2.*',
+          config = function()
+              require 'window-picker'.setup({
+                  filter_rules = {
+                      include_current_win = false,
+                      autoselect_one = true,
+                      -- filter using buffer options
+                      bo = {
+                          -- if the file type is one of following, the window will be ignored
+                          filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                          -- if the buffer type is one of following, the window will be ignored
+                          buftype = { 'terminal', "quickfix" },
+                      },
+              },
+          })
+          end,
+        },
+      },
+      config = function ()
+        require('config.neo-tree')
+      end
+    }  
+  
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
+
